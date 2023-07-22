762f97fa53b62d96dbc491833247d7e88ebf486d
Mubarak kual louis <mubaraklouis@gmail.com>
2023-07-22 13:26:57 +0200

    [Task:2] Finish the main functionality of the navbar
    I coded the foundamental feartures of the navbar am left with
    include animation when the navbar link is in hover state and
    changing the unites into rem and finally make functional links

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,19 @@
 </script>
 <template>
   <div>
-<nav class="flex justify-between mainContainer">
-  <div class="flex gap-1"><img class="smallIcons" src="/logo.png" alt=""><h1>MFlick</h1></div>
-  <div><ul class="flex navGap ">
+<nav class="sm:flex sm:justify-between md:flex md:justify-between mainContainer md:mainContainer navbar">
+  <div class="flex gap-1 logo"><img class="smallIcons" src="/logo.png" alt=""><h1>MFlick</h1></div>
+  <div><ul class="sm:flex navGap md:flex nav-items">
     <li class="captalised">TV shows</li>
     <li>Events</li>
     <li>Sports</li>
   </ul></div>
-  <div class="flex gap-1 rounded-md"><img class="smallIcons" src="/bell.png" alt=""><p>Signup</p></div>
+  <div class="flex gap-1 rounded-md signup"><img class="smallIcons" src="/bell.png" alt=""><p>Signup</p></div>
+  <div class="handbuger">
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+  </div>
 </nav>
   </div>
 </template>
