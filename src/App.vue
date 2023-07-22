8ee3677e500d108f0558b2fd706ef517a70ee7fe
Mubarak kual louis <mubaraklouis@gmail.com>
2023-07-22 14:50:16 +0200

    [Task:3] make the navbar links responsive and applied transition
    the code written will make the navbar links reponsive and apply
    transiton plus converting the px units into rem

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,18 @@
 <template>
   <div>
 <nav class="sm:flex sm:justify-between md:flex md:justify-between mainContainer md:mainContainer navbar">
-  <div class="flex gap-1 logo"><img class="smallIcons" src="/logo.png" alt=""><h1>MFlick</h1></div>
+  <div class="flex gap-1 logo"><img class="smallIcons" src="/logo.png" alt=""><h1><a href="/">MFlick</a></h1></div>
   <div><ul class="sm:flex navGap md:flex nav-items">
-    <li class="captalised">TV shows</li>
-    <li>Events</li>
-    <li>Sports</li>
+    <li><a href="#">TV shows</a>
+      <div class="border-link shadow-md"></div>
+    </li>
+    <li><a href="#">Events</a>
+    <div class="border-link shadow-md"></div></li>
+    <li><a href="#">Sports</a>
+      <div class="border-link shadow-md"></div>
+    </li>
   </ul></div>
-  <div class="flex gap-1 rounded-md signup"><img class="smallIcons" src="/bell.png" alt=""><p>Signup</p></div>
+  <div class="flex gap-1 rounded-md signup"><img class="smallIcons" src="/bell.png" alt=""><p><a href="#">Signup</a></p></div>
   <div class="handbuger">
     <div class="line"></div>
     <div class="line"></div>
