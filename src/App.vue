c978891c83a2a66cc701caa97887ba1965e4182b
Mubarak kual louis <mubaraklouis@gmail.com>
2023-08-22 17:00:52 +0300

    work on the router

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,18 @@
 <script setup lang="ts">
-
-import heroSection from './components/heroSection.vue';
+import HomeView from './views/HomeView.vue'
+import theFooter from './components/theFooter.vue';
 import theNavbar from './components/theNavbar.vue';
-import theWatchHistory from './components/theWatchHistory.vue';
-import theMovies from './components/theMovies.vue'
-import  theFooter from './components/theFooter.vue'
-
 </script>
 <template>
   <div>
- <section class="hero">
+    <section class="hero">
+      <the-navbar />
 
-<the-navbar/>
-
-<hero-section/>
-    
-
-    
-<div class="mainContainer">
-  <h3 class="uppercase">whatch history</h3>
-  <section class="mt-10 whatch-history grid grid-cols-1 md:grid md:grid-cols-4 sm:grid sm:grid-cols-2 gap-8">
-
-     <the-watch-history/>
-        </section>
-</div> 
-<!-- working on the main cards of the website -->
-<div class="recomended-movies mt-20 mainContainer ">
-<section>
-  <div class="md:flex md:justify-between sm:flex sm:justify-between mt-8 mb-8">
-  <h1 class="uppercase sm:mb-4">continue watching</h1>
-
-    <form class="movie-search mt-8 sm:mt-0 md:mt-0">
-      <input placeholder="popular" type="text">
-    </form>
-
-  </div>
-
-<theMovies/>
-
-
-</section>
-</div>
-<the-footer/>
- </section>
-<router-view>
-
-</router-view>
-
+      <!-- <HomeView/> -->
+      <router-view> </router-view>
+      <the-footer />
+    </section>
+ 
   </div>
 </template>
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import heroSection from '../components/heroSection.vue'
+import theWatchHistory from '../components/theWatchHistory.vue'
+import theMovies from '../components/theMovies.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <hero-section />
+
+<div class="mainContainer">
+  <h3 class="uppercase">whatch history</h3>
+  <section
+    class="mt-10 whatch-history grid grid-cols-1 md:grid md:grid-cols-4 sm:grid sm:grid-cols-2 gap-8"
+  >
+    <the-watch-history />
+  </section>
+</div>
+<!-- working on the main cards of the website -->
+<div class="recomended-movies mt-20 mainContainer">
+  <section>
+    <div class="md:flex md:justify-between sm:flex sm:justify-between mt-8 mb-8">
+      <h1 class="uppercase sm:mb-4">continue watching</h1>
+
+      <form class="movie-search mt-8 sm:mt-0 md:mt-0">
+        <input placeholder="popular" type="text" />
+      </form>
+    </div>
+
+    <theMovies />
+  </section>
+</div>
   </main>
 </template>
