179269182c4177ff07ffac7c343f7836dc9b134e
Mubarak kual louis <mubaraklouis@gmail.com>
2023-11-05 14:21:04 +0200

    optimous the app for production

--- src/components/default/theFooter.vue
+++ src/components/default/theFooter.vue
@@ -4,11 +4,11 @@
 
 <template>
   <div>
-    <footer class="footer-design pt-4 pb-4">
+    <footer class="pt-4 pb-4 footer-design">
       <section>
-        <div class="mainContainer mb-8 grid grid-cols-1 md:grid md:grid-cols-3 gap-x-20">
+        <div class="grid grid-cols-1 mb-8 mainContainer md:grid md:grid-cols-3 gap-x-20">
           <div>
-            <div class="flex gap-1 logo mb-4">
+            <div class="flex gap-1 mb-4 logo">
               <img class="smallIcons" src="/logo.png" alt="" />
               <h1><a href="/">MFlick</a></h1>
             </div>
@@ -20,40 +20,40 @@
               <h6 class="rating">7.9/306k</h6>
               <img class="w-7" src="/imdb.png" alt="" />
             </div>
-            <div class="flex gap-3 hero-icons mt-5">
+            <div class="flex gap-3 mt-5 hero-icons">
               <img src="/instagram.png" alt="" />
               <img src="/bell.png" alt="" />
-              <img src="github.png" alt="" />
+              <!-- <img src="github.png" alt="" /> -->
             </div>
 
             <div class="mt-4"><h6 class="text-sm">Movie-Flick copy 2023</h6></div>
-            <!-- <div class="red-btn mt-5">
+            <!-- <div class="mt-5 red-btn">
       <button class="rounded-md shadow-md">see more Details</button>
         </div> -->
           </div>
           <div class="flex gap-10 mt-8 md:mt-0">
             <ul>
-              <div class="space-y-2 mt-5">
-                <h2 class="uppercase mt-4 mb-2">Products</h2>
+              <div class="mt-5 space-y-2">
+                <h2 class="mt-4 mb-2 uppercase">Products</h2>
                 <li>
                   <a class="text-sm" href="#">TV shows</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">Events</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">Sports</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">movies</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">series</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
 
                 <br />
@@ -61,54 +61,54 @@
             </ul>
 
             <ul>
-              <div class="space-y-2 mt-5">
-                <h2 class="uppercase mt-4 mb-2">Couponds</h2>
+              <div class="mt-5 space-y-2">
+                <h2 class="mt-4 mb-2 uppercase">Couponds</h2>
                 <li>
                   <a class="text-sm" href="#">TV shows</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">Events</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">Sports</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">movies</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <li>
                   <a class="text-sm" href="#">series</a>
-                  <div class="border-link shadow-md"></div>
+                  <div class="shadow-md border-link"></div>
                 </li>
                 <br />
               </div>
             </ul>
           </div>
           <ul>
-            <div class="space-y-2 mt-5">
-              <h2 class="uppercase mt-4 mb-2">Important Links</h2>
+            <div class="mt-5 space-y-2">
+              <h2 class="mt-4 mb-2 uppercase">Important Links</h2>
               <li>
                 <a class="text-sm" href="#">Mkay_dev</a>
-                <div class="border-link shadow-md"></div>
+                <div class="shadow-md border-link"></div>
               </li>
               <li>
                 <a class="text-sm" href="#">Home</a>
-                <div class="border-link shadow-md"></div>
+                <div class="shadow-md border-link"></div>
               </li>
               <li>
                 <a class="text-sm" href="#">About</a>
-                <div class="border-link shadow-md"></div>
+                <div class="shadow-md border-link"></div>
               </li>
               <li>
                 <a class="text-sm" href="#">Contact</a>
-                <div class="border-link shadow-md"></div>
+                <div class="shadow-md border-link"></div>
               </li>
               <li>
                 <a class="text-sm" href="#">Api</a>
-                <div class="border-link shadow-md"></div>
+                <div class="shadow-md border-link"></div>
               </li>
               <br />
             </div>
--- src/components/hero_section/heroSection.vue
+++ src/components/hero_section/heroSection.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div>
-        <section class="mainContainer md:grid md:grid-cols-3 md:gap-10  grid grid-cols-1 sm:mobile-container">
+        <section class="grid grid-cols-1 mainContainer md:grid md:grid-cols-3 md:gap-10 sm:mobile-container">
 
 <div class="hero-heading ">
       <h1 class="text-bold mobile-text-head" >SPIDER</h1>
@@ -11,17 +11,18 @@
       <h6>Action and advanture movie</h6>
       <h6>After being bitten by a genetically altered spider, outcast teenager</h6>
      <div class="flex gap-2"> <h6 class="rating">7.9/306k</h6> <img  class="w-7" src="/imdb.png" alt=""></div>
-     <div class="flex gap-3 hero-icons mt-5"><img src="/instagram.png" alt="">
+     <div class="flex gap-3 mt-5 hero-icons"><img src="/instagram.png" alt="">
       <img src="/bell.png" alt="">
-      <img src="github.png" alt=""></div>
-      <div class="red-btn mt-5">
+      <!-- <img src="github.png" alt=""> -->
+    </div>
+      <div class="mt-5 red-btn">
     <button class="rounded-md shadow-md">see more Details</button>
       </div>
     </div>
-<div class="spider-image  place-self-center">
+<div class="spider-image place-self-center">
  <img src="/spider-man-png-from-pngfre-46-1.png" alt="">
     </div>
-    <div class="man-text flex items-end ">
+    <div class="flex items-end man-text ">
   <h1 class="">MAN</h1>
     </div>
   </section> 
